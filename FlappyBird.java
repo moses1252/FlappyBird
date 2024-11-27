b459c02d9659694af1ad7df87c0d614065c98436
Anthony Viramontes <47967342+moses1252@users.noreply.github.com>
2024-11-27 13:22:23 -0800

    added the bird, doesn't move yet
    bird needs to move first, then focus on collision

--- FlappyBird.java
+++ FlappyBird.java
@@ -1,11 +1,11 @@
-import javax.swing.JPanel;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 import java.awt.*;
 
-public class FlappyBird extends JPanel implements ActionListener, KeyListener {
+//implements ActionListener, KeyListener 
+public class FlappyBird extends JPanel {
     int boardWidth = 360;
     int boardHeight = 640;
 
@@ -15,16 +15,62 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     Image topPipeImg;
     Image bottomPipeImg;
 
-    // bird class
-    int birdX = boardWidth / 8;
-    int birdY = boardWidth / 2;
+    FlappyBird() {
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        // setBackground(Color.blue);
+
+        // load images
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+
+        // bird
+        bird = new Bird(birdImg);
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        // background
+        g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
+
+        // bird
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+    }
+
+    // bird, give x and y posotion
+    int birdX = boardWidth / 8; // going to place x 1/8 from the left side
+    int birdY = boardHeight / 2; // going to place y 1/2 from the top of the side
+
+    // lets give bird its width and height;
     int birdWidth = 34;
     int birdHeight = 24;
 
-    // pipe class
-    int pipeX = boardWidth;
-    int pipeY = 0;
-    int pipeWidth = 64; // scaled by 1/6
-    int pipeHeight = 512;
+    class Bird {
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+        Bird(Image img) {
+            this.img = img;
+        }
+        // we can change this bird implementation if you want
+    }
+
+    // game logic
+    Bird bird;
+
+    // // pipe class
+    // int pipeX = boardWidth;
+    // int pipeY = 0;
+    // int pipeWidth = 64; // scaled by 1/6
+    // int pipeHeight = 512;
 
 }
