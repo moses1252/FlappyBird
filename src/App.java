ba73156a74de7a67075d5da930318f7b9188a6f1
Anthony Viramontes <47967342+moses1252@users.noreply.github.com>
2024-11-26 17:40:45 -0800

    app file done for now

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null); // is going to place the window at the center of our screen
+        frame.setResizable(false);// lets make sure the user cant fix the size
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // this will termintate the program once user closes
+                                                              // program
+
     }
 }
